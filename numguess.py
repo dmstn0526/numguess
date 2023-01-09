221bcfd97904cc44d00f57ded09362dcbd161187
dmstn0526 <dmstn_0526@naver.com>
2023-01-09 16:40:01 +0900

    feat: Get guess, user name from user
    Get user name from user,
    and say Hi to user,
    and Get guess from user,
    and then print username with user's guessed number

--- numguess.py
+++ numguess.py
@@ -6,3 +6,9 @@ answer = randing(1,100)
 # print anser
 print(answer)
 
+username = input("Hi! What's your name? ")
+print(f"Hi, {username} ! Guess the answer !")
+
+# Guess number
+guessnum = input("Guess the answer (1~100) : ")
+print(f"{username} ! You guessed {guessnum} !")
