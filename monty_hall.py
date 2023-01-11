f33ef375a7c775e99270da5a100e1b71ae238563
dmstn0526 <dmstn_0526@naver.com>
2023-01-11 10:46:07 +0900

    feat: Add Interation
    for 10000 times for test

--- monty_hall.py
+++ monty_hall.py
@@ -3,17 +3,19 @@ from random import shuffle, choice
 
 N = 10000
 
-doors - [0,0,1]
-shuffle(doors)
-user_choice = choice(doors)
-
 result = {
     'stay':0,
     'change':0,
 }
 
-if user_choice == 0:
-    result['change']+=1
-if user_choice == 1:
-    result['stay']+=1
+doors = [0,0,1]
 
+for _ in range(N+1):
+    shuffle(doors)
+    user_choice = choice(doors)
+    if user_choice == 0:
+        result['change']+=1
+    if user_choice == 1:
+        result['stay']+=1
+
+print(result)
