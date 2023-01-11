82ae03eb588648adf6da04dc221bb6051cc7050e
dmstn0526 <dmstn_0526@naver.com>
2023-01-11 10:41:02 +0900

    feat: Increase result Depend on user_choice

--- monty_hall.py
+++ monty_hall.py
@@ -1,7 +1,19 @@
 from random import shuffle, choice
 
+
 N = 10000
 
 doors - [0,0,1]
 shuffle(doors)
-choice(doors)
+user_choice = choice(doors)
+
+result = {
+    'stay':0,
+    'change':0,
+}
+
+if user_choice == 0:
+    result['change']+=1
+if user_choice == 1:
+    result['stay']+=1
+
