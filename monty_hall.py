d249295d302c35a772ced34e49e89115622c8264
dmstn0526 <dmstn_0526@naver.com>
2023-01-11 10:48:30 +0900

    feat: Users to Decide Interation

--- monty_hall.py
+++ monty_hall.py
@@ -1,21 +1,21 @@
 from random import shuffle, choice
 
 
-N = 10000
-
 result = {
-    'stay':0,
-    'change':0,
+    'stay_to_win':0,
+    'move_to_win':0,
 }
 
 doors = [0,0,1]
 
-for _ in range(N+1):
+iter_num = int(input("Enter some num(100-10000): "))
+
+for _ in range(iter_num):
     shuffle(doors)
     user_choice = choice(doors)
     if user_choice == 0:
-        result['change']+=1
+        result['move_to_win']+=1
     if user_choice == 1:
-        result['stay']+=1
+        result['stay_to_win']+=1
 
 print(result)
