2ee3adc91bfd57cfd797adb55269719554b6afbd
dmstn0526 <dmstn_0526@naver.com>
2023-01-11 16:51:42 +0900

    feat: made funtion (get_start_set(), game())
    set main

--- numguess.py
+++ numguess.py
@@ -2,41 +2,55 @@ from random import randint
 from time import sleep
 
 # Generate answer
-answer = randint(1,100)
+def game_start_set():
+    answer = randint(1,100)
 
-# print anser
-print(answer)
+    # print anser
+    print(answer)
 
-username = input("Hi! What's your name? ")
-print(f"Hi, {username} ! Guess the answer !")
-print("You have 10 chance! Let's START")
-count = 0
+    username = input("Hi! What's your name? ")
+    print(f"Hi, {username} ! Guess the answer !")
+    print("You have 10 chance! Let's START")
 
-while (True):
-    count += 1
-    # Guess number
-    guessnum = int(input("Guess the answer (1~100) : "))
-    print(f"{username} ! You guessed {guessnum} !")
+    return username, answer
 
-    # Compare answer with user's guess
-    if guessnum==answer :     
-        print("****************************")
-        sleep(1)
-        print("****************************")
-        sleep(1)
-        print("****************************")
-        sleep(1)
-        print(f'You got it right!! The anwer is {answer}!!')
-        break
+def game(username, answer):
+    count = 0
+    while (True):
+        count += 1
+        # Guess number
+        guessnum = int(input("Guess the answer (1~100) : "))
+        print(f"{username} ! You guessed {guessnum} !")
 
-    elif guessnum > answer:
-        print(f'Keep going.. That was too high, {username}..')
-        
-    elif guessnum < answer:
-        print(f'keep going,, That was too low, {username}..')
-    if count == 10:
-        print('---------10 chance was done---------\n-------------You Failed-------------')
-        break
+        # Compare answer with user's guess
+        if guessnum==answer :     
+            print("****************************")
+            sleep(1)
+            print("****************************")
+            sleep(1)
+            print("****************************")
+            sleep(1)
+            print(f'You got it right!! The anwer is {answer}!!')
+            break
+
+        elif guessnum > answer:
+            print(f'Keep going.. That was too high, {username}..')
+            
+        elif guessnum < answer:
+            print(f'keep going,, That was too low, {username}..')
+        if count == 10:
+            print('---------10 chance was done---------\n-------------You Failed-------------')
+            break
+    return
+
+if __name__=='__main__':
+    username,answer = game_start_set()
+    game(username, answer)
+
+# game_start_set()
+# game()
+
+
 
 
 
